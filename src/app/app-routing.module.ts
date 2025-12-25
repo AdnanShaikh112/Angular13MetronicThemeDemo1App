2661412725393409aadfc8942c59675617ce137b
Adnan Shaikh <adnan.shaikh@zobiwebsolutions.com>
2025-12-25 19:35:09 +0530

    Removed unnecessary pages and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { EcommerceComponent } from './dashboards/ecommerce/ecommerce.component';
 
 const routes: Routes = [
   // { path: '', redirectTo: 'products', pathMatch: 'full' },
   // { path: '**', redirectTo: 'products' }
-  { path: 'dashboards/default', component: AppComponent },
-  { path: 'dashboards/ecommerce', component: EcommerceComponent }
+  { path: 'dashboards/default', component: AppComponent }
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterLink, RouterModule } from '@angular/router';
-import { EcommerceComponent } from './dashboards/ecommerce/ecommerce.component';
-import { ProjectsComponent } from './dashboards/projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    EcommerceComponent,
-    ProjectsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
