c1d4dac0004a16ed3972655284b30be10b16f8c2
Adnan Shaikh <adnan.shaikh@zobiwebsolutions.com>
2025-12-25 18:09:38 +0530

    Added Dashboard Default and ECommerce Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { EcommerceComponent } from './dashboards/ecommerce/ecommerce.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', redirectTo: 'products', pathMatch: 'full' },
+  // { path: '**', redirectTo: 'products' }
+  { path: 'dashboards/default', component: AppComponent },
+  { path: 'dashboards/ecommerce', component: EcommerceComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterLink, RouterModule } from '@angular/router';
+import { EcommerceComponent } from './dashboards/ecommerce/ecommerce.component';
+import { ProjectsComponent } from './dashboards/projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EcommerceComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
